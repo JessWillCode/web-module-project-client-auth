cc5faada1fbe5b215cd1588166754e997fb1bdbb
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-01-25 18:53:02 -0500

    finished logout component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Link, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
 import AddFriends from './components/AddFriends';
+import Logout from './components/Logout';
 
 function App() {
 const [friends, setFriends] = useState([]);
@@ -18,14 +19,15 @@ const [friends, setFriends] = useState([]);
               <Link to='/login'>LOGIN</Link>
               <Link to='/friends-list'>FRIENDS LIST</Link>
               <Link to='/add-friend'>ADD FRIEND</Link>
-              <button>LOGOUT</button>
+              <Link to='/logout'>LOGOUT</Link>
           </nav>
       </header>
       <Switch>
-      <Route path='/add-friend' render={() =>(<AddFriends friends={friends} setFriends={setFriends}/>)} />
-      <Route path='/friends-list' render={() =>(<FriendsList friends={friends} setFriends={setFriends}/>)} />
-      <Route path='/login' component={Login} />
-      <Route path='/' component={Login} />
+        <Route path='/logout' component={Logout} />
+        <Route path='/add-friend' render={() =>(<AddFriends friends={friends} setFriends={setFriends}/>)} />
+        <Route path='/friends-list' render={() =>(<FriendsList friends={friends} setFriends={setFriends}/>)} />
+        <Route path='/login' component={Login} />
+        <Route path='/' component={Login} />
       </Switch>
     </div>
   );
---
+++ src/components/Logout.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+export default function Logout() {
+    const { push } = useHistory();
+
+    useEffect(() => {
+        axiosWithAuth()
+        .post('/logout')
+        .then(res => {
+            localStorage.removeItem('token');
+            push('/login');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, []);
+
+    return <div></div>;
+}
